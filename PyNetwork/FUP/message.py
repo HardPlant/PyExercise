ecebecefc87550251925c98e3ccd52b8d8e7a852
unknown <120897@live.jnu.ac.kr>
2017-06-29 18:43:19 +0900

    message struct

---
+++ PyNetwork/FUP/message.py
@@ -0,0 +1,37 @@
+REQ_FILE_SEND = 0x01
+REP_FILE_SEND = 0x02
+FILE_SEND_DATA = 0x03
+FILE_SEND_RES = 0x04
+
+NOT_FRAGMENTED = 0x00
+FRAGMENTED = 0x01
+
+NOT_LASTMSG = 0x00
+LASTMSG = 0x01
+
+DENIED = 0x00
+ACCEPTED = 0x01
+
+FAIL = 0x00
+SUCCESS = 0x01
+
+class ISerializble: # Data-> Byte Array, len(ByteArray)
+    def GetBytes(self):
+        pass
+    def GetSize():
+        pass
+
+class Message(ISerializble):
+    def __init__(self):
+        self.Header = ISerializble()
+        self.Body = ISerializble()
+
+    def GetBytes(self):
+        buffer = bytes(self.GetSize())
+        header = self.Header.GetBytes()
+        body = self.Body.GetBytes()
+
+        return header + body
+
+    def GetSize(self):
+        return self.Header.GetSize() + self.Body.GetSize()
