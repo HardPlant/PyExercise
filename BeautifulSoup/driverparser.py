6d839ff02bcd61df9251659619a231081a9987a9
unknown <120897@live.jnu.ac.kr>
2017-07-16 17:18:14 +0900

    json?

--- BeautifulSoup/driverparser.py
+++ BeautifulSoup/driverparser.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 import os
+import json
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 driver = webdriver.Chrome(os.path.join(BASE_DIR, 'chromedriver.exe'))
 
 #url에 접근한다.
-driver.get('https://google.com')
+driver.get('https://nid.naver.com/nidlogin.login')
+
+account = open(os.path.join(BASE_DIR, 'config.json')).read()
+data = json.loads(account)
+print(data)
