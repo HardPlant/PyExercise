022280b8c19fb25c6b2ded1ad2ac31c0d1464d16
unknown <120897@live.jnu.ac.kr>
2017-07-16 15:52:43 +0900

    login with session

--- BeautifulSoup/parser.py
+++ BeautifulSoup/parser.py
@@ -1,12 +1,20 @@
 import requests
-from bs4 import BeautifulSoup
-import json
-import os
+from bs4 import BeautifulSoup as bs
+
 LOGIN_INFO = {
-    'userId' : , 
-    'userPassword' :
+    'userId' : 'myidid', 
+    'userPassword':'mypassword123'
 }
 
 with requests.Session() as s:
-    req = s.post('https://www.clien.net/service/login', data=LOGIN_INFO)
+    first_page = s.get('https://www.clien.net/service')
+    html = first_page.text
+    soup = bs(html, 'html.parser')
+    csrf = soup.find('input',{'name' : '_csrf'}) # input -> name = "_csrf"
+    print(csrf['value']) # input name="_csrf value="-""
+    
+    LOGIN_INFO = {**LOGIN_INFO, **{'_csrf' : csrf['value']}}
+    #python3 dict concat + {**dict1, **dict2}
+
+    login_req = s.post('https://www.clien.net/service/login', data=LOGIN_INFO)
     print(login_req.status_code)
