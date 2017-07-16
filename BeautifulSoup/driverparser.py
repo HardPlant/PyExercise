548ed46191826af5ba6ba665d949c1fa5e9e6ff4
unknown <120897@live.jnu.ac.kr>
2017-07-16 17:57:55 +0900

    unholy password input parser.

--- BeautifulSoup/driverparser.py
+++ BeautifulSoup/driverparser.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from getpass import getpass
 import os
 import json
 
@@ -12,9 +13,9 @@ driver = webdriver.Chrome(os.path.join(BASE_DIR, 'chromedriver.exe'))
 
 #url에 접근한다.
 driver.get('https://nid.naver.com/nidlogin.login')
-
 driver.find_element_by_name('id').send_keys(data['naverid'])
-driver.find_element_by_name('pw').send_keys(data['naverpw'])
+print("Enter Password of " + data['naverid'] +": ")
+driver.find_element_by_name('pw').send_keys(getpass())
 #Presses Login Button.
 driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 driver.get('https://order.pay.naver.com/home')
