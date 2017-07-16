89a543c0343070135aac4caa4255210771d40be8
unknown <120897@live.jnu.ac.kr>
2017-07-16 14:57:50 +0900

    blog text href crawling

--- BeautifulSoup/parser.py
+++ BeautifulSoup/parser.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 req = requests.get('https://beomi.github.io/beomi.github.io_old/')
 
@@ -6,7 +7,17 @@ html = req.text
 header = req.headers
 status = req.status_code
 is_ok = req.ok
-print(html)
-print(header)
-print(status)
-print(is_ok)
+# BeautifulSoup으로 html소스를 python객체로 변환하기
+# 첫 인자는 html소스코드, 두 번째 인자는 어떤 parser를 이용할지 명시.
+# 이 글에서는 Python 내장 html.parser를 이용했다.
+soup = BeautifulSoup(html, 'html.parser')
+my_titles = soup.select(
+    'h3 > a'
+    )
+
+# my_titles는 list 객체
+for title in my_titles:
+    # Tag안의 텍스트
+    print(title.text)
+    # Tag의 속성을 가져오기(ex: href속성)
+    print(title.get('href'))
