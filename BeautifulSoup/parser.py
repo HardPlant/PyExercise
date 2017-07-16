a26bdb3dabfb0f8642dc3c163a0cde1e615fdbd9
unknown <120897@live.jnu.ac.kr>
2017-07-16 15:19:47 +0900

    json dump parser

--- BeautifulSoup/parser.py
+++ BeautifulSoup/parser.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 req = requests.get('https://beomi.github.io/beomi.github.io_old/')
 
@@ -14,10 +18,16 @@ soup = BeautifulSoup(html, 'html.parser')
 my_titles = soup.select(
     'h3 > a'
     )
-
+data = {}
 # my_titles는 list 객체
 for title in my_titles:
+    '''
     # Tag안의 텍스트
     print(title.text)
     # Tag의 속성을 가져오기(ex: href속성)
     print(title.get('href'))
+    '''
+    data[title.text] = title.get('href')
+
+with open(os.path.join(BASE_DIR, 'result.json'), 'w+') as json_file:
+    json.dump(data,json_file)
